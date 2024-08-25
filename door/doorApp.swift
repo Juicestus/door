65bbe5c9ea2d0f158ad1b9501f8bee00faf6f927
Juicestus <jus@gtsbr.org>
2024-08-25 12:09:13 -0500

    Initial Commit

---
+++ door/doorApp.swift
@@ -0,0 +1,17 @@
+//
+//  doorApp.swift
+//  door
+//
+//  Created by Justus Languell on 8/25/24.
+//
+
+import SwiftUI
+
+@main
+struct doorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
