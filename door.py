ca2952fdbc9f0c1df015827706cfc27f1f4fb857
Juicestus <jus@gtsbr.org>
2024-08-26 12:18:20 -0500

    "Firmware"

---
+++ door.py
@@ -0,0 +1,71 @@
+import firebase_admin
+import time
+import threading
+#import RPi.GPIO as gpio
+
+from enum import Enum
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+class Door:
+    def __init__(self, pin_open, pin_close):
+        self.is_open = False
+        self.pin_open = pin_open
+        self.pin_close = pin_close
+        #gpio.setup(self.pin_open, GPIO.OUT)
+        #gpio.setup(self.pin_close, GPIO.OUT)
+
+    def set_pin_for(self, pin_to_set, t_s):
+        print(f' Wrote pin {pin_to_set} = HIGH.')
+        #gpio.output(pin_to_set, gpio.HIGH)
+        time.sleep(t_s)
+        print(f' Wrote pin {pin_to_set} = LOW.')
+        #gpio.output(pin_to_set, gpio.LOW)
+
+    def open_pls(self):
+        print('Opening door...')
+        self.set_pin_for(self.pin_open, 5)
+        self.is_open = True
+
+    def close_pls(self):
+        print('Closing door...')
+        self.set_pin_for(self.pin_close, 5)
+        self.is_open = False
+
+class Net:
+    def __init__(self, db):
+        self.doc_ref = db.collection('req').document('req')
+        self.doc_ref.on_snapshot(self.on_snapshot)
+        self.last_ts = -1
+
+    def on_snapshot(self, doc_snapshot, changes, read_time):
+        for doc in doc_snapshot:
+            if doc.id != "req": continue
+            if self.last_ts == -1: #ignore the first time
+                self.last_ts = 0
+            else:
+                self.last_ts = time.time()
+                print(f'Open request recived at timestamp {self.last_ts}.')
+
+if __name__ == '__main__':
+    print('Initializing...')
+
+    cred = credentials.Certificate("auth.json")
+    app = firebase_admin.initialize_app(cred)
+    db = firestore.client()
+
+    door = Door(22, 23)
+    net = Net(db)
+    
+    time.sleep(5)
+    print(' done.')
+
+    while True:
+        pls_open = (time.time() - net.last_ts) < 15
+
+        if pls_open and not door.is_open:
+            door.open_pls()
+        if not pls_open and door.is_open:
+            door.close_pls()
+
+        time.sleep(0.05)
